0706662a8b8509f8bda86a83aac9b278c9a4e7fc
Druid3000 <qwedc156ft>
2020-05-05 13:18:39 +0300

    Add card service

---
+++ src/main/java/com/druid3000/FlashCards/service/CardService.java
@@ -0,0 +1,25 @@
+package com.druid3000.FlashCards.service;
+
+import com.druid3000.FlashCards.entity.Card;
+import com.druid3000.FlashCards.repository.CardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class CardService {
+
+    private final CardRepository cardRepository;
+
+    @Transactional
+    public Card addCard(Card card){
+        return cardRepository.save(card);
+    }
+
+    public Card findById(Integer id){
+        return cardRepository.findById(id).orElse(new Card());
+    }
+}
+
